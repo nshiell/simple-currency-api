4a2ade906d36051e61c47d216f49deea11a06c5d
Nicholas Shiell <nicholas@nshiell.com>
2020-03-09 17:24:47 +0000

    Routing

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,50 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
-    //return $router->app->version();
-    return view('page', ['name' => 'James']);
+    return view('page');
+});
+
+/** @todo make this a DELETE request not a GET */
+// this call will clear the exchange rates cache
+$router->get('/api/cache/clear', function () {
+    app(\App\Rate\RateCacheClear::class)->deleteAll();
+
+    return response()->json([
+        'error' => 0,
+        'msg'   => 'ok'
+    ]);
+});
+
+// this call will return a basic text string
+$router->get('/api/exchange/info', function () {
+    return response()->json([
+        'error' => 0,
+        'msg'   => env('ABOUT_INFO')
+    ]);
+});
+
+// this call will convert 100 usd into euros, and return the amount
+$router->get('/api/exchange/{value}/{from}/{to}', function ($from, $to, $value) {
+    try {
+        $rate = app(\App\Factories\RateFactory::class)->createRate($from, $to);
+    } catch (\UnexpectedValueException $e) {
+        return response()->json([
+            'error' => 1,
+            'msg'   => $e->getMessage()
+        ])->setStatusCode(404);
+    }
+
+    return response()->json([
+        'error'     => 0,
+        'amount'    => $rate->getValueExchanged($value),
+        'fromCache' => (int) $rate->getIsFromCache()
+    ]);
+});
+
+// Any invalid urls submitted to the API must return
+$router->get('/{all:.*}', function () {
+    return response()->json([
+        'error' => 1,
+        'msg'   => 'invalid request'
+    ])->setStatusCode(404);
 });
